1c58597942bd1d8065b948349bb4a00c659519ae
JuanLuisHercules <juan.hernandez@uees.edu.sv>
2026-05-28 14:28:11 -0600

    Actualización del proyecto42

--- modulos/frmDescriptor/services/descriptorService.js
+++ modulos/frmDescriptor/services/descriptorService.js
@@ -1,18 +1,17 @@
 // Servicio del Descriptor - Gestión de datos
 var DescriptorService = {
     getAll: function() {
-        const data = localStorage.getItem('descriptores');
+        var data = localStorage.getItem('descriptores');
         return data ? JSON.parse(data) : [];
     },
     
     save: function(descriptor) {
-        const descriptors = this.getAll();
+        var descriptors = this.getAll();
         descriptor.id = descriptors.length + 1;
-        descriptor.codigo = `DES-${(descriptors.length + 1).toString().padStart(4, '0')}`;
+        descriptor.codigo = 'DES-' + (descriptors.length + 1).toString().padStart(4, '0');
         descriptor.estado = 'BORRADOR';
         descriptor.fechaCreacion = new Date().toISOString();
         
-        // Inicializar auditoría
         descriptor.auditoria = {
             creado: {
                 fecha: new Date().toISOString(),
@@ -25,52 +24,68 @@ var DescriptorService = {
         };
         
         descriptors.push(descriptor);
-        localStorage.setItem('descriptores', JSON.stringify(descriptores));
+        localStorage.setItem('descriptores', JSON.stringify(descriptors));
         return descriptor;
     },
     
     update: function(id, data) {
-        const descriptors = this.getAll();  // CORREGIDO: usar this.getAll()
-        const index = descriptors.findIndex(d => d.id === id);
-        if (index !== -1) {
-            // Si se está actualizando el estado, registrar en auditoría
-            if (data.estado && descriptors[index].estado !== data.estado) {
-                if (!descriptors[index].auditoria) {
-                    descriptors[index].auditoria = { historial: [] };
-                }
-                if (!descriptors[index].auditoria.historial) {
-                    descriptors[index].auditoria.historial = [];
-                }
-                descriptors[index].auditoria.historial.push({
-                    fecha: new Date().toISOString(),
-                    estadoAnterior: descriptors[index].estado,
-                    estadoNuevo: data.estado,
-                    accion: 'CAMBIO DE ESTADO'
-                });
+        var descriptors = this.getAll();
+        var index = -1;
+        for (var i = 0; i < descriptors.length; i++) {
+            if (descriptors[i].id === id) {
+                index = i;
+                break;
             }
+        }
+        if (index !== -1) {
             descriptors[index] = { ...descriptors[index], ...data };
-            localStorage.setItem('descriptores', JSON.stringify(descriptores));
+            localStorage.setItem('descriptores', JSON.stringify(descriptors));
             return descriptors[index];
         }
         return null;
     },
     
     getById: function(id) {
-        return this.getAll().find(d => d.id === id);
+        var descriptors = this.getAll();
+        for (var i = 0; i < descriptors.length; i++) {
+            if (descriptors[i].id === id) {
+                return descriptors[i];
+            }
+        }
+        return null;
     },
     
     getByCreador: function(creador) {
-        return this.getAll().filter(d => d.creador === creador);
+        var descriptors = this.getAll();
+        var resultado = [];
+        for (var i = 0; i < descriptors.length; i++) {
+            if (descriptors[i].creador === creador) {
+                resultado.push(descriptors[i]);
+            }
+        }
+        return resultado;
     },
     
     getByEstado: function(estado) {
-        return this.getAll().filter(d => d.estado === estado);
+        var descriptors = this.getAll();
+        var resultado = [];
+        for (var i = 0; i < descriptors.length; i++) {
+            if (descriptors[i].estado === estado) {
+                resultado.push(descriptors[i]);
+            }
+        }
+        return resultado;
     },
     
-    // Registrar evento en auditoría
     registrarEvento: function(id, evento) {
-        const descriptors = this.getAll();
-        const index = descriptors.findIndex(d => d.id === id);
+        var descriptors = this.getAll();
+        var index = -1;
+        for (var i = 0; i < descriptors.length; i++) {
+            if (descriptors[i].id === id) {
+                index = i;
+                break;
+            }
+        }
         if (index !== -1) {
             if (!descriptors[index].auditoria) {
                 descriptors[index].auditoria = { historial: [] };
@@ -94,9 +109,8 @@ var DescriptorService = {
         return false;
     },
     
-    // Obtener auditoría completa
     getAuditoria: function(id) {
-        const descriptor = this.getById(id);
+        var descriptor = this.getById(id);
         if (descriptor) {
             return descriptor.auditoria || { historial: [] };
         }
