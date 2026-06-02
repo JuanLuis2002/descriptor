4283cd1486cac3e1a9d402f2db0f2286071c9aec
JuanLuisHercules <juan.hernandez@uees.edu.sv>
2026-06-02 09:46:38 -0600

    Actualización del proyecto103

--- js/app.js
+++ js/app.js
@@ -100,8 +100,8 @@ function loadMenu() {
         `);
     } else if (currentUser.rol === 'TH_GENERALISTA') {
         nav.append(`
-            <a href="#" class="nav-link text-white px-3 py-2" data-modulo="complementarTH">
-                <i class="fas fa-pen me-2"></i> <span>Complementar Descriptor</span>
+            <a href="#" class="nav-link text-white px-3 py-2" data-modulo="revisionTH">
+                <i class="fas fa-check-double me-2"></i> <span>Revisión Técnica</span>
             </a>
         `);
     } else if (currentUser.rol === 'JEFE_TH') {
